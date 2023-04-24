6d87c8aeb065ee40e64d4fb2928157a57d6a513e
Ivã Munhoz <iamunhoz@gmail.com>
2023-04-23 21:50:38 -0300

    fix port II

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { getPersonRepository } from './database/person';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World');
