0dd4ae16eaecb4b0a35abf2a2ff39ab6f64ab7d2
Ivã Munhoz <iamunhoz@gmail.com>
2023-04-24 18:32:39 -0300

    setting up redis

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,22 +1,23 @@
-/* import { createClient } from 'redis';
+import { createClient } from 'redis';
+
+const { REDISUSER, REDISPASSWORD, REDISHOST, REDISPORT } = process.env
+
+const REDIS_RAILWAIL_SERVER_URL = `redis://${REDISUSER}:${REDISPASSWORD}@${REDISHOST}:${REDISPORT}`
 
 export const startRedisServer = async (): Promise<void> => {
   const client = createClient({
     //redis[s]://[[username][:password]@][host][:port][/db-number]
-    url: 'redis://localhost:6379',
+    url: REDIS_RAILWAIL_SERVER_URL//'redis://localhost:6379',
   });
 
   client.on('error', (err) => console.log('Redis Client Error', err));
 
   await client.connect();
 
-  await client.set('first_object', { chaveUm: 'teste' });
-
+  // await client.set('first_object', { chaveUm: 777 });
   const pattern = '*';
   const serverKeys = await client.keys(pattern);
 
   console.log('serverKeys', serverKeys);
 };
- */
 
-export {}
