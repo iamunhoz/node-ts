fedebead17407165055b8556069db38fad50c720
Ivã Munhoz <iamunhoz@gmail.com>
2023-04-25 19:33:28 -0300

    format rome

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,32 @@
-import express from 'express';
-import os from 'os';
-import cors from 'cors';
-import { startRedisServer } from './database';
+import { startRedisServer } from "./database"
+import cors from "cors"
+import express from "express"
+import os from "os"
 
-const app = express();
+const app = express()
 
-app.use(cors());
+app.use(cors())
 
 const host: { ip: string; port: number } =
-  os.hostname() === 'krafter' ?
-  { ip: '127.0.1', port: 3000 } :
-  { ip: '0.0.0.0', port: process.env.PORT };
+	os.hostname() === "krafter"
+		? { ip: "127.0.1", port: 3000 }
+		: { ip: "0.0.0.0", port: process.env.PORT }
 
-const redisController = startRedisServer();
+const redisController = startRedisServer()
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+app.get("/", (req, res) => {
+	res.send("Hello World")
+})
 
-app.get('/add-new-value/', async (req, res) => {
-  await redisController.connect();
+app.get("/add-new-value/", async (req, res) => {
+	await redisController.connect()
 
-  await redisController.set(req.query.key as string, req.query.value as string)
+	await redisController.set(req.query.key as string, req.query.value as string)
 
-  const value = await redisController.get(req.query.key as string)
-  res.send(`new value created in db: ${value}`)
+	const value = await redisController.get(req.query.key as string)
+	res.send(`new value created in db: ${value}`)
 })
 
 app.listen(host.port, host.ip, () => {
-  console.log(`Server listening on port ${host.port}`);
-});
+	console.log(`Server listening on port ${host.port}`)
+})
---
+++ src/prisma.tutorial.md
@@ -0,0 +1,15 @@
+**add** prisma as dev-dep
+**run** 'npx prisma init --datasource-provider postgresql'
+**change** url of postgresql db to railway host variable
+**install** prisma extension
+**create** first model - user (at 7:30)
+id Int @id @default(autoincrement())
+name String
+**run** npx prisma migrate dev --name init
+**install** @prisma/client
+**run** npx prisma generate
+**create** script file
+**create** async function in script file to connect to db (at 11:00)
+
+_tutorial link_
+https://www.youtube.com/watch?v=RebA5J-rlwg
